fe98fe15f6993436908aebe77b99e023194703b8
Amakiri David <120092804+Amakiri201@users.noreply.github.com>
2023-12-07 09:06:28 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -47,7 +47,7 @@ function handleSubmit() {
   <div class="wrapper">
     <div class="hero">
       <h1>GOJO</h1>
-      <p>The simplest to-do app ever</p>
+      <p>A simplest to-do app </p>
     </div>
 
     <form @submit.prevent="handleSubmit">
